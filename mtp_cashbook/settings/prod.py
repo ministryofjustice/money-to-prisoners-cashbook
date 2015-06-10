1c4d592585cf5f647ef35b292c1050bb321b11f1
Marco Fucci <info@marcofucci.com>
2015-06-10 16:56:24 +0100

    Add localhost to ALLOWED_HOSTS as django errors saying that it needs it

--- mtp_cashbook/settings/prod.py
+++ mtp_cashbook/settings/prod.py
@@ -14,9 +14,8 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# TODO: Set your ALLOWED_HOSTS
-
 ALLOWED_HOSTS = [
+    'localhost',
     '*.dsd.io',
     '*.service.gov.uk'
 ]
