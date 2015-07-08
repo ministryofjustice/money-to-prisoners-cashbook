a965c0c0716d37306a9990dad24c7f3c39ff2c38
Mat Harden <matharden@gmail.com>
2015-07-08 23:37:52 +0100

    Highlight payments rows when checked

--- mtp_cashbook/assets-src/javascripts/app.js
+++ mtp_cashbook/assets-src/javascripts/app.js
@@ -1,1 +1,21 @@
-// Your scripts go here
+/* global $ */
+(function(){
+  'use strict';
+
+  var selectAll = $('#select-all'),
+      checks = $('.check :checkbox');
+
+  selectAll.on('change', function() {
+    var all = this;
+
+    checks.each(function() {
+      this.checked = all.checked;
+      $(this).change();
+    });
+  });
+
+  checks.on('change', function() {
+    var check = $(this);
+    check.closest('tr')[check.is(':checked') ? 'addClass' : 'removeClass']('is-selected');
+  })
+})();
