2ad7f518d245b60eff3bde2272d9f30195f45588
Steve Marshall <steve@stevemarshall.com>
2015-07-09 22:57:50 +0100

    Fix pep8 indentation errors
    Also remove unnecessary `urlpatterns` examples.
    
    Linting errors reported by flake8 that this fixes:
    
        ./mtp_cashbook/apps/cashbook/forms.py:46:12: E121 continuation line under-indented for hanging indent
        ./mtp_cashbook/apps/mtp_auth/forms.py:44:25: E126 continuation line over-indented for hanging indent
        ./mtp_cashbook/apps/mtp_auth/forms.py:46:21: E121 continuation line under-indented for hanging indent
        ./mtp_cashbook/apps/mtp_auth/urls.py:8:13: E126 continuation line over-indented for hanging indent
        ./mtp_cashbook/apps/mtp_auth/urls.py:9:9: E121 continuation line under-indented for hanging indent
        ./mtp_cashbook/apps/mtp_auth/urls.py:10:5: E124 closing bracket does not match visual indentation

--- mtp_cashbook/apps/mtp_auth/urls.py
+++ mtp_cashbook/apps/mtp_auth/urls.py
@@ -4,7 +4,8 @@ from django.core.urlresolvers import reverse_lazy
 
 urlpatterns = patterns('',
     url(r'^login/$', 'mtp_auth.views.login', name='login'),
-    url(r'^logout/$', 'mtp_auth.views.logout', {
+    url(
+        r'^logout/$', 'mtp_auth.views.logout', {
             'next_page': reverse_lazy('auth:login')
         }, name='logout'
     ),
