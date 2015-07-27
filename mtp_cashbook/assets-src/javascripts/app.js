2e4cc1843d1a45b4cc5eecfccf8229ccd3392977
Mat Harden <matharden@gmail.com>
2015-07-27 15:38:14 +0100

    Extract feature tour highlight to external repo

--- mtp_cashbook/assets-src/javascripts/app.js
+++ mtp_cashbook/assets-src/javascripts/app.js
@@ -19,82 +19,6 @@
     check.closest('tr')[check.is(':checked') ? 'addClass' : 'removeClass']('is-selected');
   });
 
-  hopscotch.highlight = {
-
-    settings: {
-      padding: 7
-    },
-
-    getStep: function() {
-      var stepNo = hopscotch.getCurrStepNum();
-      return tour.steps[stepNo];
-    },
-
-    getTarget: function(step) {
-      var target = step.target;
-      return $(typeof target === 'string' ? '#' + target : target);
-    },
-
-    show: function() {
-      var step = hopscotch.highlight.getStep();
-      var target = hopscotch.highlight.getTarget(step);
-
-      target.addClass('hopscotch-highlighted');
-
-      hopscotch.highlight.showHighlight(target, step.table);
-      hopscotch.highlight.showOverlay();
-    },
-
-    remove: function() {
-      $('.hopscotch-highlighted').removeClass('hopscotch-highlighted');
-      $('#hopscotch-overlay, #hopscotch-highlight').remove();
-    },
-
-    showHighlight: function(el, table) {
-
-      hopscotch.highlight.positionHighlight(el, table);
-
-      // Highlight entire table column
-      if (typeof table !== 'undefined') {
-        // Bring each cell to foreground
-        var i = el.index();
-        el.closest('table').find('tbody tr').each(function() {
-          $(this).find('td').eq(i).addClass('hopscotch-highlighted');
-        });
-      }
-    },
-
-    positionHighlight: function(el, table) {
-      var h = $('<div/>').attr('id', 'hopscotch-highlight').appendTo('body');
-
-      h.css({
-        'top': el.offset().top,
-        'left': el.offset().left,
-        'width': el.outerWidth(),
-        'height': el.outerHeight()
-      });
-
-      if (typeof table !== 'undefined') {
-        // Resize highlight to column height
-        h.css('height', el.closest('table').height());
-      }
-    },
-
-    showOverlay: function() {
-      $('<div/>').attr('id', 'hopscotch-overlay').appendTo('body');
-    }
-
-  };
-
-  window.onresize = function() {
-    // reposition highlight
-    var step = hopscotch.highlight.getStep();
-    var target = hopscotch.highlight.getTarget(step);
-    hopscotch.highlight.positionHighlight(target, step.table);
-  };
-
-  var tour;
-
   var tourBase = {
     i18n: {
       closeTooltip: 'Dismiss'
@@ -113,7 +37,7 @@
       Cookies.set('hopscotch.state.' + tour.id, 'dismissed');
 
       if (tour.steps[stepNo].dismissTo) {
-        setTimeout(function(){
+        window.setTimeout(function(){
           hopscotch.startTour(tour, tour.steps[stepNo].dismissTo);
         }, 300);
       }
@@ -126,7 +50,7 @@
       }
 
       if (tour.steps[stepNo].fadeout) {
-        setTimeout(function(){
+        window.setTimeout(function(){
           $('.hopscotch-bubble').fadeOut(300, function(){
             hopscotch.endTour(false);
           });
@@ -144,7 +68,7 @@
   };
 
   if (typeof tourSteps !== 'undefined') {
-    tour = _.extend(tourBase, tourSteps);
+    var tour = window.tour = _.extend(tourBase, tourSteps);
 
     var runTour = function(){
       hopscotch.startTour(tour, parseInt(Cookies.get('hopscotch.' + tour.id) || 0));
