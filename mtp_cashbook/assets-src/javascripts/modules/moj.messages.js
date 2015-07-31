bc689148e64c7a69480412b02fc2e4e7dea7e89c
Dom Smith <dom@teneightfive.com>
2015-07-31 12:00:32 +0100

    Add JS module to focus on flash messages if present Allows screenreaders to annouce the message

---
+++ mtp_cashbook/assets-src/javascripts/modules/moj.messages.js
@@ -0,0 +1,30 @@
+// Messages module
+// Dependencies: moj, _, jQuery
+
+/* global moj, _, $ */
+(function () {
+  'use strict';
+
+  moj.Modules.Messages = {
+    selector: '.messages[role=alert]',
+
+    init: function () {
+      _.bindAll(this, 'render');
+      this.bindEvents();
+      this.render(null, {wrap: 'body'});
+    },
+
+    bindEvents: function () {
+      moj.Events.on('Messages.render', this.render);
+    },
+
+    render: function (e, params) {
+      var $el = $(this.selector, $(params.wrap));
+
+      if ($el.length > 0) {
+        $el.focus();
+      }
+    }
+  };
+
+})();
