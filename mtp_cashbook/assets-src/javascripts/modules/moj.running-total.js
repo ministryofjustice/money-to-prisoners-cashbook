896be40b0e63acff2b818b716c6ea074463cbbca
Dom Smith <dom@teneightfive.com>
2015-08-13 14:30:56 +0100

    Add running total to batch process view

---
+++ mtp_cashbook/assets-src/javascripts/modules/moj.running-total.js
@@ -0,0 +1,67 @@
+// Running Total module
+// Dependencies: moj, _, jQuery
+
+(function () {
+  'use strict';
+
+  moj.Modules.RunningTotal = {
+    selector: '.js-RunningTotal',
+
+    init: function () {
+      _.bindAll(this, 'render', 'updateTotal');
+      this.cacheEls();
+
+      if (this.$totalsContainer.length) {
+        this.bindEvents();
+        this.render();
+        this.updateTotal();
+      }
+    },
+
+    cacheEls: function () {
+      this.$items = $('.js-RunningTotal-item');
+      this.$totalsContainer = $(this.selector);
+      this.label = this.$totalsContainer.data('label');
+
+      this.$label = $('<p>');
+      this.$total = $('<strong>');
+    },
+
+    bindEvents: function () {
+      moj.Events.on('RunningTotal.render', this.render);
+      this.$items.on('change', this.updateTotal);
+    },
+
+    updateTotal: function () {
+      this.$total.html('&pound;' + this.calculateTotal());
+      this.$label
+        .text(this.label)
+        .append(this.$total);
+    },
+
+    calculateTotal: function () {
+      var total = 0;
+
+      this.$items.each(function (i, el) {
+        var $el = $(el);
+        var amount = $el.data('amount');
+
+        if ($el.is(':checked')) {
+          total += parseFloat(amount);
+        }
+      });
+
+      return total.toFixed(2);
+    },
+
+    render: function () {
+      this.$label
+        .attr('aria-live', 'polite')
+        .attr('aria-atomic', 'false');
+
+      this.$totalsContainer
+        .append(this.$label);
+    }
+  };
+
+})();
