10576e95052ef4d25a2ec85cb08ef1bd4adc64f9
Dom Smith <dom@teneightfive.com>
2015-08-14 12:01:13 +0100

    Add Dialog module Allows you to create a modal/dialog from a link in the document or trigger it from a different module

---
+++ mtp_cashbook/assets-src/javascripts/modules/moj.dialog.js
@@ -0,0 +1,101 @@
+// Dialog module
+// Dependencies: moj, _, jQuery
+
+/* globals console */
+(function () {
+  'use strict';
+
+  moj.Modules.Dialog = {
+    selector: '.js-Dialog',
+
+    init: function () {
+      _.bindAll(this, 'render', 'closeDialog', 'onKeyUp');
+      this.cacheEls();
+      this.bindEvents();
+    },
+
+    cacheEls: function () {
+      this.$body = $('body');
+      this.$backdrop = $('<div>').addClass('Dialog-backdrop');
+      this.$close = $('<a>');
+
+      // close element
+      this.$close
+        .attr('href', '#')
+        .addClass('Dialog-close');
+    },
+
+    bindEvents: function () {
+      moj.Events.on('Dialog.render', this.render);
+      this.$body.on('click', this.selector, this.render);
+    },
+
+    render: function (e) {
+      this.$triggerEl = $(e.target);
+
+      e.preventDefault();
+      this.openDialog(this.$triggerEl.attr('href'));
+    },
+
+    openDialog: function (target) {
+      var $dialog = $(target);
+
+      // log warning if target doesn't exist
+      if ($dialog.length === 0) {
+        moj.log('Modules.Dialog Warning: The target element, ' + target + ', does not exist.');
+        return;
+      }
+
+      if ($dialog.data('close-label')) {
+        this.$close.text($dialog.data('close-label'));
+      } else {
+        this.$close.text('close');
+      }
+
+      $dialog
+        .attr('open', '')
+        .attr('tabindex', '-1')
+        .append(this.$close)
+        .focus();
+
+      this.$body
+        .prepend(this.$backdrop);
+
+      // bind close events
+      this.$body.on('click.Dialog', '.Dialog-close, .Dialog-backdrop', this.closeDialog);
+      this.$body.on('keyup.Dialog', this.onKeyUp);
+    },
+
+    closeDialog: function (e) {
+      var $dialog = this.$body.find('dialog[open]');
+
+      if (e) {
+        e.preventDefault();
+      }
+
+      $dialog
+        .removeAttr('open')
+        .removeAttr('tabindex');
+
+      this.$close
+        .remove();
+
+      this.$backdrop
+        .remove();
+
+      this.$triggerEl.focus();
+
+      // unbind close events
+      this.$body.off('.Dialog');
+    },
+
+    onKeyUp: function (e) {
+      e = e || window.event;
+
+      if (e.keyCode === 27) {
+        this.closeDialog();
+      }
+    }
+  };
+
+})();
