b9ed9d12fd92c5e3960e82165e73c12722379db5
Dom Smith <dom@teneightfive.com>
2015-08-27 16:51:17 +0100

    Make sign in view inline with rest of UI - Add basic validation pattern - Restructure markup for error handling

--- mtp_cashbook/apps/core/templatetags/core.py
+++ mtp_cashbook/apps/core/templatetags/core.py
@@ -11,3 +11,14 @@ def to_string(value):
 @register.filter(is_safe=True)
 def safewrap(val, arg):
     return val.format(arg)
+
+
+@register.filter(name='addclass')
+def addclass(element, className):
+   return element.as_widget(attrs={"class":className})
+
+
+@register.filter()
+def field_from_name(form, name):
+    if name in form.fields:
+        return form[name]
