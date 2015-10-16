19b4f4e0d90ee103529c793382138bc341bb6718
Igor <igor.ushkarev@digital.justice.gov.uk>
2015-10-16 14:24:12 +0100

    enables API date formatter to parse dates as well as date-times

--- mtp_cashbook/apps/cashbook/templatetags/format_string_date.py
+++ mtp_cashbook/apps/cashbook/templatetags/format_string_date.py
@@ -1,19 +1,23 @@
 from django import template
 from django.conf import settings
 from django.utils.dateformat import format
-from django.utils.dateparse import parse_datetime
+from django.utils.dateparse import parse_date, parse_datetime
 
 register = template.Library()
 
 
 @register.filter
-def format_string_date(date_string, format_string=settings.DATETIME_FORMAT):
+def format_string_date(date_string, datetime_format=settings.DATETIME_FORMAT, date_format=settings.DATE_FORMAT):
     """
-    MTP API responds with UTC date strings,
-    this filter formats it according to the DATETIME_FORMAT settings
+    MTP API responds with UTC date/time strings,
+    this filter formats it according to the DATETIME_FORMAT/DATE_FORMAT settings
     """
-    try:
-        datetime = parse_datetime(date_string)
-        return format(datetime, format_string)
-    except (ValueError, TypeError):
-        return ''
+    for parser, format_string in [[parse_datetime, datetime_format], [parse_date, date_format]]:
+        try:
+            parsed_date = parser(date_string)
+            if not parsed_date:
+                continue
+            return format(parsed_date, format_string)
+        except (ValueError, TypeError):
+            continue
+    return ''
