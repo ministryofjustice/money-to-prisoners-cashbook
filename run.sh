2ebc758bd921b1ada3c55912d3398a05099b7ce6
Max Froumentin <max.froumentin@digital.justice.gov.uk>
2015-12-01 18:18:49 +0000

    Add a makefile and a script to do the same as gulp, but simpler - the makefile should be launched for various build and run tasks - the script shouldn't normally be launched on its own but it started by the makefile - jshint slightly corrected to allow JS files to pass linting

---
+++ run.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# This script runs the application in various ways depending on the params
+# passed. It's normally run from the makefile
+
+IMAGES_SRC_DIR=./node_modules/money-to-prisoners-common/assets/images
+IMAGES_DST_DIR=./mtp_cashbook/assets/images
+
+# kill all processes spawned by this script in the
+# background when it stops, in particular django
+# restarts
+function clean_up {
+  echo "Killing all spawned processes."
+  kill -- -$(ps -o pgid= $PID | grep -o [0-9]*)
+  exit 1
+}
+
+# Activate and run the django application
+function start {
+  source venv/bin/activate
+  ./manage.py runserver 8001
+}
+
+case "$1" in
+  start)
+    # just run normally
+    start
+    ;;
+  watch)
+    # run normally but monitor assets and recompile
+    # them when they change
+    trap clean_up INT
+    start & PID=$!
+    shift
+    fswatch -o $@ | xargs  -n1 -I{} make -f makefile_frontend all
+    ;;
+  serve)
+    # as above but also run browser-sync for dynamic
+    # browser reload
+    trap clean_up INT
+    start & PID=$!
+    browser-sync start --host=localhost --port=3000 --proxy=localhost:8001 --no-open --ui-port=3001 &
+    shift
+    fswatch -o $@ | xargs  -n1 -I{} sh -c 'make -f makefile_frontend all; browser-sync reload'
+    ;;
+  *)
+    echo "Usage: $0 [start|watch|serve] <args>"
+    echo " - $0 start: just start the application server"
+    echo " - $0 watch <list of asset directories to monitor>: start the "
+    echo "   application server and recompile the assets if they are changed"
+    echo " - $0 serve <list of asset directories to monitor>: start the "
+    echo "   browser-sync server and recompile the assets if they are changed"
+    exit 1
+esac
