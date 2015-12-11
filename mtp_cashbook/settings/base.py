522dc3c06f36a5398415ee931e093cb94f28e248
Igor <igor.ushkarev@digital.justice.gov.uk>
2015-12-11 12:49:27 +0000

    Remove redundant "debug" template context processor

--- mtp_cashbook/settings/base.py
+++ mtp_cashbook/settings/base.py
@@ -98,7 +98,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'moj_utils.context_processors.debug',
                 'moj_utils.context_processors.analytics',
             ],
         },
