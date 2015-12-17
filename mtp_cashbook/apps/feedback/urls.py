47920b80ea79be5a121e7f46bd70b378bfe40f35
Ian Brechin <ian.brechin@gmail.com>
2015-12-17 10:03:24 +0000

    Provide url for a post-feedback submission success page
    This page will confirm that the ticket was submitted and
    provide a link back to where the user was when they decided
    to submit feedback.

--- mtp_cashbook/apps/feedback/urls.py
+++ mtp_cashbook/apps/feedback/urls.py
@@ -1,13 +1,18 @@
 from django.conf import settings
 from django.conf.urls import url
+from django.core.urlresolvers import reverse_lazy
 from zendesk_tickets import views
 
 urlpatterns = [
     url(r'^feedback/$', views.ticket,
         {
             'template_name': 'feedback/submit_feedback.html',
-            'success_redirect_url': '',
+            'success_redirect_url': reverse_lazy('feedback_success'),
             'subject': 'MTP Cashbook Feedback',
             'tags': ['feedback', 'mtp', 'cashbook', settings.ENVIRONMENT]
         }, name='submit_ticket'),
+    url(r'^feedback/success$', views.success,
+        {
+            'template_name': 'feedback/success.html',
+        }, name='feedback_success'),
 ]
