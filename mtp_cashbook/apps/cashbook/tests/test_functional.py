75e27a4bbfb86a4e7b9a8f87c27da75c6ffc8c95
Igor <igor.ushkarev@digital.justice.gov.uk>
2016-01-04 17:20:10 +0000

    Install PhantomJS and Selenium dependencies for python functional tests

--- mtp_cashbook/apps/cashbook/tests/test_functional.py
+++ mtp_cashbook/apps/cashbook/tests/test_functional.py
@@ -2,9 +2,20 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-class TestTwo(LiveServerTestCase):
 
-    def __login(self, username, password):
+class FunctionalTestCase(LiveServerTestCase):
+    @classmethod
+    def _databases_names(cls, include_mirrors=True):
+        # this app has no databases
+        return []
+
+    def setUp(self):
+        self.driver = webdriver.PhantomJS(executable_path='./node_modules/.bin/phantomjs')
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def _login(self, username, password):
         self.driver.get(self.live_server_url)
         login_field = self.driver.find_element_by_id('id_username')
         login_field.send_keys(username)
@@ -17,33 +28,12 @@ class TestTwo(LiveServerTestCase):
         self.assertEquals('Money sent to prisoners', heading.text)
         self.assertEquals('32px', heading.value_of_css_property('font-size'))
 
-
     def test_bad_login(self):
-        self.__login('test-prison-1', 'bad-password')
+        self._login('test-prison-1', 'bad-password')
         self.assertIn('There was a problem submitting the form',
                       self.driver.page_source)
 
     def test_good_login(self):
-        self.__login('test-prison-1', 'test-prison-1')
+        self._login('test-prison-1', 'test-prison-1')
         self.assertEquals(self.driver.current_url, self.live_server_url + '/')
         self.assertIn('Credits to process', self.driver.page_source)
-
-
-
-    def setUp(self):
-        self.driver = webdriver.PhantomJS()
-
-    def tearDown(self):
-        self.driver.quit()
-
-    def _pre_setup(self, **kwargs):
-        """ Override the test database creation """
-        pass
-
-    def _post_teardown(self, **kwargs):
-        """ Override the test database deletion """
-        pass
-
-
-if __name__ == '__main__':
-    unittest.main()
