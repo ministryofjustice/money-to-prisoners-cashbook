7f0b6a534ae30948202c037d6bcd0087326c5940
Igor <igor.ushkarev@digital.justice.gov.uk>
2016-01-08 17:39:12 +0000

    Update run script to share more functionality and allow parameter passing

--- run.sh
+++ run.sh
@@ -1,20 +1,31 @@
 #!/usr/bin/env bash
-if [ "$#" -eq 0 ]; then
-  echo "Usage: $0 [start|watch|serve|clean|build] [args]"
-  echo " - start [port]: start the application server using the port specified"
-  echo " - watch [port]: start the application server and recompile the assets when they change"
-  echo " - serve [port]: start the browser-sync server and recompile the assets when they change"
-  echo " - build: compile all the assets"
-  echo " - test: run the test suite"
-else
-  if [ ! -d node_modules ]; then
-    echo "The installation process is about to start. It usually takes a while."
-    echo "The only thing that this script doesn't do is set up the API. While"
-    echo "installation is running, head to https://github.com/ministryofjustice/money-to-prisoners-api"
-    echo "to find out how to run it."
-    echo "Press Return to continue"
-    read
-    npm install
-  fi
-  make -f node_modules/money-to-prisoners-common/Makefile $1 app=mtp_cashbook port=${2-8001}
+
+cd `dirname $0`
+
+if [ ! -d node_modules ]; then
+  echo "The installation process is about to start. It usually takes a while."
+  echo "The only thing that this script doesn't do is set up the API. While"
+  echo "installation is running, head to https://github.com/ministryofjustice/money-to-prisoners-api"
+  echo "to find out how to run it."
+
+  read -p "Proceed with installation? [Y/n]: " PROCEED
+  case ${PROCEED} in
+    [nN]*) exit 1;;
+  esac
+
+  npm install
 fi
+
+load_defaults() {
+  # all constants and default values should be defined here
+  APP=cashbook
+  PROJECT_PATH=`pwd`
+  SHARED_MAKEFILE=node_modules/money-to-prisoners-common/Makefile
+  DEFAULT_PORT=8001
+  DEFAULT_BROWSERSYNC_PORT=3001
+  DEFAULT_BROWSERSYNC_UI_PORT=3031
+  DEFAULT_DJANGO_SETTINGS_MODULE=mtp_api.settings
+}
+
+. node_modules/money-to-prisoners-common/tasks.sh
+main $@
