1263e2b51004e80cb86e98cc97c1d88e43c381df
Igor <igor.ushkarev@digital.justice.gov.uk>
2016-01-12 11:39:21 +0000

    Point the run.sh script directly to the shared make file …removing the included shell tasks entirely

--- run.sh
+++ run.sh
@@ -7,25 +7,13 @@ if [ ! -d node_modules ]; then
   echo "The only thing that this script doesn't do is set up the API. While"
   echo "installation is running, head to https://github.com/ministryofjustice/money-to-prisoners-api"
   echo "to find out how to run it."
-
-  read -p "Proceed with installation? [Y/n]: " PROCEED
-  case ${PROCEED} in
-    [nN]*) exit 1;;
-  esac
-
   npm install
 fi
 
-load_defaults() {
-  # all constants and default values should be defined here
-  APP=cashbook
-  PROJECT_PATH=`pwd`
-  SHARED_MAKEFILE=node_modules/money-to-prisoners-common/Makefile
-  DEFAULT_PORT=8001
-  DEFAULT_BROWSERSYNC_PORT=3001
-  DEFAULT_BROWSERSYNC_UI_PORT=3031
-  DEFAULT_DJANGO_SETTINGS_MODULE=mtp_api.settings
-}
-
-. node_modules/money-to-prisoners-common/tasks.sh
-main $@
+make -f node_modules/money-to-prisoners-common/Makefile \
+  command_script=`basename $0` \
+  app=cashbook \
+  port=8001 \
+  browsersync_port=3001 \
+  browsersync_ui_port=3031 \
+  $@
