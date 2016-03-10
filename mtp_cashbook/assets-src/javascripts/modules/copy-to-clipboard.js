d06a7e67dde9cbe8ffe53e364182e593d571eb7a
Max Froumentin <max.froumentin@digital.justice.gov.uk>
2016-03-10 16:54:10 +0000

    Make copy-to-clipboard work in Chrome, and add hint bubbles

--- mtp_cashbook/assets-src/javascripts/modules/copy-to-clipboard.js
+++ mtp_cashbook/assets-src/javascripts/modules/copy-to-clipboard.js
@@ -3,12 +3,34 @@
 
 exports.CopyToClipboard = {
   init: function () {
+    $('.copy-to-clipboard').on('mouseover', function() {
+      var bubble = $(this).next('span');
+      bubble.removeClass('copied').text('Copy prisoner number to clipboard').show();
+    });
+
+    $('.copy-to-clipboard').on('mouseout', function() {
+      var bubble = $(this).next('span');
+      bubble.hide();
+    });
+
     $('.copy-to-clipboard').on('click', function() {
+      var bubble = $(this).next('span');
       var copyText = $(this).prev('span').text();
-      var holdText = document.getElementById('holdText');
-      holdText.innerText = copyText;
-      var copied = holdText.createTextRange();
-      copied.execCommand('copy');
+      if (document.body.createTextRange) {
+        var holdText = document.getElementById('holdText');
+        holdText.innerText = copyText;
+        var copied = holdText.createTextRange();
+        copied.execCommand('copy');
+      } else {
+        var textField = document.createElement('textarea');
+        textField.innerText = copyText;
+        document.body.appendChild(textField);
+        textField.select();
+        document.execCommand('copy');
+        $(textField).remove();
+      }
+      bubble.addClass('copied').text('Copied!').show();
+      setTimeout(function() { bubble.hide(); }, 500);
     });
   }
 };
