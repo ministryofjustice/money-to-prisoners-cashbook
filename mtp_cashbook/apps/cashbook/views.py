7b40cae592bcbbefafbb151fc2270247a99c0ec5
Ian Brechin <ian.brechin@gmail.com>
2016-04-25 14:19:33 +0100

    Add feature flag to hide debit card specific UI

--- mtp_cashbook/apps/cashbook/views.py
+++ mtp_cashbook/apps/cashbook/views.py
@@ -1,5 +1,6 @@
 import logging
 
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.core.urlresolvers import reverse_lazy, reverse
@@ -211,6 +212,7 @@ class TransactionHistoryView(FormView):
             'page_range': page_range,
             'page_count': page_count,
             'transaction_owner_name': self.request.user.get_full_name,
+            'DEBIT_CARD_ACTIVE': settings.DEBIT_CARD_ACTIVE
         })
         return context
 
