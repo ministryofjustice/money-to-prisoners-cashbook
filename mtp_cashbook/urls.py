8cd7dc4dcdca1741946b8ce9086390060d418d13
Igor <igor.ushkarev@digital.justice.gov.uk>
2016-10-26 12:20:25 +0100

    Add plain error HTML pages which nginx uses internally …these unfortunately must be exposed as real URLs

--- mtp_cashbook/urls.py
+++ mtp_cashbook/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.http import HttpResponse
+from django.template.response import TemplateResponse
 from django.views.generic.base import RedirectView
 
 from moj_irat.views import HealthcheckView, PingJsonView
@@ -20,6 +21,9 @@ urlpatterns = [
 
     url(r'^favicon.ico$', RedirectView.as_view(url=settings.STATIC_URL + 'images/favicon.ico', permanent=True)),
     url(r'^robots.txt$', lambda request: HttpResponse('User-agent: *\nDisallow: /', content_type='text/plain')),
+
+    url(r'^404.html$', lambda request: TemplateResponse(request, 'mtp_common/errors/404.html', status=404)),
+    url(r'^500.html$', lambda request: TemplateResponse(request, 'mtp_common/errors/500.html', status=500)),
 ]
 
 handler404 = 'mtp_common.views.page_not_found'
