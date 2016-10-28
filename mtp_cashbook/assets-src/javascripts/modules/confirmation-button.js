c76ea7265a7baa0b3f87524000b727d7ae14cc24
Vicky Gray <vicky.gray@digital.justice.gov.uk>
2016-10-28 16:43:06 +0100

    Correct javascript variables

--- mtp_cashbook/assets-src/javascripts/modules/confirmation-button.js
+++ mtp_cashbook/assets-src/javascripts/modules/confirmation-button.js
@@ -9,16 +9,16 @@ exports.ConfirmationButton = {
     $('.mtp-confirm__button').hide();
 
     $(document).on('change', '.Checkbox', function() {
-      this.$items = $('.js-RunningTotal-item').length;
-      this.$completeItems = $('.js-RunningTotal-item:checked').length;
+      var items = $('.js-RunningTotal-item').length;
+      var completeItems = $('.js-RunningTotal-item:checked').length;
 
-      if (this.$completeItems > 0) {
+      if (completeItems > 0) {
         $('.mtp-confirm__button').show();
       } else {
         $('.mtp-confirm__button').hide();
       };
 
-      if (this.$items === this.$completeItems) {
+      if (items === completeItems) {
         $('#control-total, #entered-total').addClass('green');
       } else {
         $('#control-total, #entered-total').removeClass('green');
