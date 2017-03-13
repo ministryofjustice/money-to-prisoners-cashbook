abedd18cd7c8c570f975d8ffaf0fca226eae2642
Igor <igor.ushkarev@digital.justice.gov.uk>
2017-03-13 17:26:28 +0000

    Get user prisons from user data rather than calling an API (whose response is changing)

--- mtp_cashbook/apps/feedback/forms.py
+++ mtp_cashbook/apps/feedback/forms.py
@@ -1,5 +1,4 @@
 from django.utils.text import slugify
-from mtp_common.auth.api_client import get_connection
 from mtp_common.auth.exceptions import Unauthorized
 from slumber.exceptions import HttpClientError
 from zendesk_tickets.forms import EmailTicketForm
@@ -8,8 +7,7 @@ from zendesk_tickets.forms import EmailTicketForm
 class PrisonTicketForm(EmailTicketForm):
     def submit_ticket(self, request, subject, tags, ticket_template_name, **kwargs):
         try:
-            client = get_connection(request)
-            accessible_prisons = sorted(prison['name'] for prison in client.prisons.get()['results'])
+            accessible_prisons = sorted(prison['name'] for prison in request.user.user_data.get('prisons', []))
             subject = '%(subject)s (%(accessible_prisons)s)' % {
                 'subject': subject,
                 'accessible_prisons': ', '.join(accessible_prisons)
