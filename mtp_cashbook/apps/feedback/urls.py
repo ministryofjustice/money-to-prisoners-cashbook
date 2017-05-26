d8aa031f0c2a7e21e9e47d71ac548b3570ca37c7
Igor <igor.ushkarev@digital.justice.gov.uk>
2017-05-26 15:28:15 +0100

    Add feedback form to footer on new redesigned pages

--- mtp_cashbook/apps/feedback/urls.py
+++ mtp_cashbook/apps/feedback/urls.py
@@ -1,19 +1,27 @@
 from django.conf import settings
 from django.conf.urls import url
 from django.core.urlresolvers import reverse_lazy
+from mtp_common.feedback.views import FeedbackFooterView
 from zendesk_tickets import views
 
 from .forms import PrisonTicketForm
 
+ticket_subject = 'MTP Cashbook Feedback'
+ticket_tags = ['feedback', 'mtp', 'cashbook', settings.ENVIRONMENT]
 urlpatterns = [
     url(r'^feedback/$', views.ticket,
         {
             'form_class': PrisonTicketForm,
             'template_name': 'mtp_common/feedback/submit_feedback.html',
             'success_redirect_url': reverse_lazy('feedback_success'),
-            'subject': 'MTP Cashbook Feedback',
-            'tags': ['feedback', 'mtp', 'cashbook', settings.ENVIRONMENT],
+            'subject': ticket_subject,
+            'tags': ticket_tags,
         }, name='submit_ticket'),
+    url(r'^footer-feedback/$', FeedbackFooterView.as_view(
+        form_class=PrisonTicketForm,
+        subject=ticket_subject,
+        tags=ticket_tags,
+    ), name='submit_footer_feedback'),
     url(r'^feedback/success/$', views.success,
         {
             'template_name': 'mtp_common/feedback/success.html',
