1297ad0cd47c9bec2fdcfd2772fba478d043301f
Ian Brechin <ian.brechin@gmail.com>
2017-08-04 11:25:07 +0100

    Update functional tests for NOMIS integrated cashbook

--- mtp_cashbook/apps/cashbook/tests/test_functional.py
+++ mtp_cashbook/apps/cashbook/tests/test_functional.py
@@ -1,5 +1,6 @@
+from datetime import date
 import logging
-import re
+import os
 
 from mtp_common.test_utils import silence_logger
 from mtp_common.test_utils.functional_tests import FunctionalTestCase
@@ -14,14 +15,21 @@ class CashbookTestCase(FunctionalTestCase):
     """
     auto_load_test_data = True
     accessibility_scope_selector = '#content'
+    username = 'test-hmp-leeds'
 
     def load_test_data(self):
         with silence_logger(name='mtp', level=logging.WARNING):
-            super().load_test_data()
+            super().load_test_data(command=b'load_nomis_test_data')
 
-    def login(self, *args, **kwargs):
-        kwargs.setdefault('url', self.live_server_url + '/en-gb/')
-        super().login(*args, **kwargs)
+    def login(self, username, password, url=None,
+              username_field='id_username', password_field='id_password'):
+        """
+        Fill in login form
+        """
+        self.driver.get(url or self.live_server_url + '/en-gb/')
+        self.click_on_text('Sign into cashbook')
+        self.type_in(username_field, username)
+        self.type_in(password_field, password, send_return=True)
 
     def click_logout(self):
         """
@@ -31,7 +39,8 @@ class CashbookTestCase(FunctionalTestCase):
         self.driver.find_element_by_css_selector('#proposition-links li:last-child a').click()
 
     def login_and_go_to(self, link_text, substring=False):
-        self.login('test-prison-1', 'test-prison-1')
+        self.set_change_notification_read_cookie(self.username)
+        self.login(self.username, self.username)
         if substring:
             self.click_on_text_substring(link_text)
         else:
@@ -51,6 +60,12 @@ class CashbookTestCase(FunctionalTestCase):
     def click_on_link(self, text):
         self.driver.find_element_by_link_text(text).click()
 
+    def set_change_notification_read_cookie(self, username):
+        self.driver.get(self.live_server_url)
+        self.driver.execute_script(
+            'document.cookie = "change-notification-read=%s; path=/;"' % username
+        )
+
 
 class LoginTests(CashbookTestCase):
     """
@@ -60,61 +75,34 @@ class LoginTests(CashbookTestCase):
     def test_title(self):
         self.driver.get(self.live_server_url + '/en-gb/')
         heading = self.driver.find_element_by_tag_name('h1')
-        self.assertEqual('Digital cashbook\nSign in', heading.text)
+        self.assertEqual('Process money sent to your prison', heading.text)
 
     def test_bad_login(self):
-        self.login('test-prison-1', 'bad-password')
+        self.login('test-hmp-leeds', 'bad-password')
         self.assertInSource('There was a problem')
         self.assertShowingView('login')
 
     def test_good_login(self):
-        self.login('test-prison-1', 'test-prison-1')
-        self.assertCurrentUrl('/en-gb/')
-        self.assertShowingView('dashboard')
+        self.login('test-hmp-leeds', 'test-hmp-leeds')
+        self.assertCurrentUrl('/en-gb/change-notification/')
+        self.assertShowingView('change-notification')
+
+    def test_good_login_with_change_notification_cookie(self):
+        self.set_change_notification_read_cookie('test-hmp-leeds')
+        self.login('test-hmp-leeds', 'test-hmp-leeds')
+        self.assertCurrentUrl('/en-gb/new/')
+        self.assertShowingView('new-credits')
 
     def test_good_login_without_case_sensitivity(self):
-        self.login('Test-PRISON-1', 'test-prison-1')
-        self.assertCurrentUrl('/en-gb/')
-        self.assertShowingView('dashboard')
+        self.login('test-HMP-leeds', 'test-hmp-leeds')
+        self.assertCurrentUrl('/en-gb/change-notification/')
+        self.assertShowingView('change-notification')
 
     def test_logout(self):
-        self.login('test-prison-1', 'test-prison-1')
+        self.login('test-hmp-leeds', 'test-hmp-leeds')
         self.click_logout()
         self.assertCurrentUrl('/en-gb/login/')
         self.assertShowingView('login')
-
-
-class InProgressPageTests(CashbookTestCase):
-    """
-    Tests for locked credits page
-    """
-
-    def setUp(self):
-        super().setUp()
-        self.login_and_go_to('Return these to new credits')
-
-    def test_going_to_the_locked_credits_page(self):
-        self.assertInSource('Currently being entered into NOMIS')
-        self.assertInSource('Staff name')
-        self.assertInSource('Time in progress')
-
-    @silence_logger(name='mtp', level=logging.WARNING)
-    def test_releasing_credits(self):
-        self.get_element('//tbody//input[@type="checkbox"][1]/following-sibling::label').click()
-        self.click_on_text('Return these to new credits')
-        self.assertCurrentUrl('/en-gb/dashboard-unlocked-payments/')
-        self.assertInSource('You have now returned')
-
-    def test_help_popup(self):
-        help_box_heading = self.driver.find_element_by_css_selector('.mtp-disclosure')
-        self.assertCssProperty('.mtp-disclosure__contents', 'display', 'none')
-        self.assertEqual('false', help_box_heading.get_attribute('aria-expanded'))
-        self.click_on_link('Help')
-        self.assertCssProperty('.mtp-disclosure__contents', 'display', 'block')
-        self.assertEqual('true', help_box_heading.get_attribute('aria-expanded'))
-        self.click_on_link('Help')
-        self.assertCssProperty('.mtp-disclosure__contents', 'display', 'none')
-        self.assertEqual('false', help_box_heading.get_attribute('aria-expanded'))
 
 
 class NewCreditsPageTests(CashbookTestCase):
@@ -124,252 +112,103 @@ class NewCreditsPageTests(CashbookTestCase):
 
     def setUp(self):
         super().setUp()
-        self.login_and_go_to('Enter next', substring=True)
+        self.set_change_notification_read_cookie(self.username)
+        self.login(self.username, self.username)
 
     def test_going_to_the_credits_page(self):
-        self.assertInSource('New credits')
-        self.assertInSource('Control total')
+        self.assertShowingView('new-credits')
 
-    def test_submitting_credits_credited(self):
+    def test_submitting_partial_batch(self):
         self.select_first_credit()
-        self.click_on_text('Confirm entered in NOMIS')
+        self.click_on_text('Credit to NOMIS')
         self.assertIsNotNone(self.driver.find_element_by_xpath(
             '//div[contains(@class, "mtp-dialogue")]'
             '/header[contains(text(), "Do you want to submit only the selected credits?")]'
         ))
 
-    @silence_logger(name='mtp', level=logging.WARNING)
-    def test_submitting_and_confirming_partial_batch(self):
-        self.select_first_credit()
-        self.click_on_text('Confirm entered in NOMIS')
-        self.click_on_text('Yes')
-        self.assertInSource('You’ve added 1 credit to NOMIS.')
-        self.assertEqual('Digital cashbook', self.driver.title)
-
     def test_submitting_and_not_confirming_partial_batch(self):
         self.select_first_credit()
-        self.click_on_text('Confirm entered in NOMIS')
+        self.click_on_text('Credit to NOMIS')
         self.click_on_text('No, continue processing')
         self.assertEqual('New credits – Digital cashbook', self.driver.title)
 
-    def test_printing(self):
-        self.click_on_text('Print these credits')
-        self.assertIsNotNone(self.driver.find_element_by_xpath(
-            '//div[contains(@class, "mtp-dialogue")]'
-            '/header[contains(text(), "Do you need to print?")]'
-        ))
-
-    def test_help_popup(self):
-        help_box_heading = self.driver.find_element_by_css_selector('.mtp-disclosure')
-        self.assertCssProperty('.mtp-disclosure__contents', 'display', 'none')
-        self.assertEqual('false', help_box_heading.get_attribute('aria-expanded'))
-        self.click_on_link('How to use this with NOMIS')
-        self.assertCssProperty('.mtp-disclosure__contents', 'display', 'block')
-        self.assertEqual('true', help_box_heading.get_attribute('aria-expanded'))
-        self.click_on_link('How to use this with NOMIS')
-        self.assertCssProperty('.mtp-disclosure__contents', 'display', 'none')
-        self.assertEqual('false', help_box_heading.get_attribute('aria-expanded'))
-
-    def test_go_back_home(self):
-        self.click_on_text('Home')
-        self.assertEqual('Digital cashbook', self.driver.title)
-        self.assertCurrentUrl('/en-gb/dashboard-batch-discard/')
+    @silence_logger(name='mtp', level=logging.WARNING)
+    def test_submitting_and_confirming_partial_batch(self):
+        self.select_first_credit()
+        self.click_on_text('Credit to NOMIS')
+        self.click_on_text('Yes')
+        if os.environ.get('DJANGO_TEST_REMOTE_INTEGRATION_URL', None):
+            try:
+                self.assertInSource('Digital cashbook is crediting to NOMIS')
+                self.driver.implicitly_wait(15)
+            except AssertionError:
+                self.assertInSource('Digital cashbook has finished crediting to NOMIS')
+            self.click_on_text('Continue')
+        self.assertInSource('1 credit sent to NOMIS')
 
     @silence_logger(name='mtp', level=logging.WARNING)
     def test_submitting_complete_batch(self):
         self.select_all_credits()
-        self.click_on_text('Confirm entered in NOMIS')
-        self.assertEqual('Digital cashbook', self.driver.title)
-        self.assertInSource('You’ve added')
+        self.click_on_text('Credit to NOMIS')
+        if os.environ.get('DJANGO_TEST_REMOTE_INTEGRATION_URL', None):
+            try:
+                self.assertInSource('Digital cashbook is crediting to NOMIS')
+                self.driver.implicitly_wait(20)
+            except AssertionError:
+                self.assertInSource('Digital cashbook has finished crediting to NOMIS')
+            self.click_on_text('Continue')
+        self.assertInSource('credits sent to NOMIS')
 
 
-class VisualTests(CashbookTestCase):
+class ProcessedCreditsPageTests(CashbookTestCase):
     """
-    Tests that need to be run with a visual browser as they require interacting
-    with browser controls (alerts or onbeforeunload)
-    """
-    required_webdrivers = ('chrome', 'firefox')
-
-    def setUp(self):
-        super().setUp()
-        self.driver.implicitly_wait(10)
-
-    def test_leaving_confirming_incomplete_batch(self):
-        # we need firefox as this is using a native dialog
-        self.login_and_go_to('Enter next', substring=True)
-        self.select_first_credit()
-        self.click_on_text('Home')
-        self.driver.switch_to.alert.dismiss()
-        self.assertEqual('New credits - Digital cashbook', self.driver.title)
-
-    def test_leaving_not_confirming_incomplete_batch(self):
-        # we need firefox as this is using a native dialog
-        self.login_and_go_to('Enter next', substring=True)
-        self.select_first_credit()
-        self.click_on_text('Home')
-        self.driver.switch_to.alert.accept()
-        self.assertEqual('Digital cashbook', self.driver.title)
-
-    def test_search_focus(self):
-        def is_search_input_focused():
-            element_tag, element_id = self.driver.execute_script('return [document.activeElement.tagName, '
-                                                                 'document.activeElement.id];')
-            return element_tag == 'INPUT' and element_id == 'id_search'
-
-        self.login_and_go_to('View all credits')
-        self.assertTrue(is_search_input_focused())
-        self.type_in('id_start', 'Today')
-        self.click_on_text('Search')
-        self.assertFalse(is_search_input_focused())
-
-    def test_go_home_with_back_button(self):
-        self.login_and_go_to('Enter next', substring=True)
-        self.driver.execute_script('window.history.go(-1)')
-        self.assertCurrentUrl('/en-gb/')
-
-
-@silence_logger(name='mtp', level=logging.WARNING)
-class Journeys(CashbookTestCase):
-    """
-    These aren't real tests but rather simulations of complex
-    sessions as a real user would do. This is used to automatically
-    populate analytics with semi-realistic data
-    """
-    required_webdrivers = ('chrome', 'firefox')
-
-    # Route: 1, 2
-    def test_journey_1(self):
-        self.login('test-prison-1', 'test-prison-1')
-        self.click_on_text_substring('Enter next')
-        self.select_all_credits()
-        self.click_on_text('Confirm entered in NOMIS')
-        self.assertCurrentUrl('/en-gb/dashboard-batch-complete/')
-
-    # Route: 1, 3
-    def test_journey_2(self):
-        self.login('test-prison-1', 'test-prison-1')
-        self.click_on_text_substring('Enter next')
-        self.click_on_text('Home')
-        self.assertCurrentUrl('/en-gb/dashboard-batch-discard/')
-
-    # Route: 1, 4, 5
-    def test_journey_3(self):
-        self.login('test-prison-1', 'test-prison-1')
-        self.click_on_text_substring('Enter next')
-        self.select_first_credit()
-        self.click_on_text('Confirm entered in NOMIS')
-        self.click_on_text('Yes')
-        self.assertCurrentUrl('/en-gb/dashboard-batch-incomplete/')
-
-    # Route: 1, 4, 6
-    def test_journey_4(self):
-        self.login('test-prison-1', 'test-prison-1')
-        self.click_on_text_substring('Enter next')
-        self.select_first_credit()
-        self.click_on_text('Confirm entered in NOMIS')
-        self.click_on_text('No, continue processing')
-        self.assertCurrentUrl('/en-gb/batch/')
-
-    # Route 1, 7, 8
-    def test_journey_5(self):
-        self.login('test-prison-1', 'test-prison-1')
-        self.click_on_text_substring('Enter next')
-        self.select_first_credit()
-        self.click_on_text('Home')
-        self.driver.switch_to.alert.accept()
-        self.assertCurrentUrl('/en-gb/dashboard-batch-discard/')
-
-    # Route 1, 7, 9
-    def test_journey_6(self):
-        self.login('test-prison-1', 'test-prison-1')
-        self.click_on_text_substring('Enter next')
-        self.select_first_credit()
-        self.click_on_text('Home')
-        self.driver.switch_to.alert.dismiss()
-        self.assertCurrentUrl('/en-gb/batch/')
-
-    def test_journey_7(self):
-        self.login('test-prison-1', 'test-prison-1')
-        self.click_on_text_substring('Enter next')
-        self.click_on_text('How to use this with NOMIS')
-        self.click_on_text('Home')
-
-    def test_journey_8(self):
-        self.login('test-prison-1', 'test-prison-1')
-        self.click_on_text_substring('Enter next')
-        self.click_on_text('How to use this with NOMIS')
-        self.click_on_text('How to use this with NOMIS')
-        self.click_on_text('Home')
-
-    def test_journey_9(self):
-        self.login('test-prison-1', 'test-prison-1')
-        self.click_on_text_substring('Enter next')
-        self.click_on_text('How to use this with NOMIS')
-        self.click_on_text('How to use this with NOMIS')
-        self.click_on_text('Home')
-
-    def test_journey_10(self):
-        self.login('test-prison-1', 'test-prison-1')
-        self.click_on_text_substring('Enter next')
-        self.click_on_text('Home')
-        self.click_logout()
-
-    def test_journey_11(self):
-        self.login('test-prison-1', 'test-prison-1')
-        self.click_on_text_substring('Enter next')
-        self.driver.execute_script('window.history.go(-1)')
-        self.click_logout()
-
-    def test_journey_12(self):
-        self.login('test-prison-1', 'test-prison-1')
-        self.click_on_text('Return these to new credits')
-        self.click_on_text('Release')
-
-    def test_journey_13(self):
-        self.login('test-prison-1', 'test-prison-1')
-        self.click_on_text('Return these to new credits')
-        self.get_element('//tbody//input[@type="checkbox"][1]/following-sibling::label').click()
-        self.click_on_text('Release')
-
-
-class HistoryPageTests(CashbookTestCase):
-    """
-    Tests for History page
+    Tests for processed credits page
     """
 
     def setUp(self):
         super().setUp()
-        self.login_and_go_to('View all credits')
+        self.login_and_go_to('Processed credits')
 
-    def get_search_button(self):
-        button = self.driver.find_element_by_xpath('//input[@value="Search"]')
+    def get_filter_button(self):
+        button = self.driver.find_element_by_xpath('//input[@value="Filter list"]')
         self.assertIsNotNone(button)
         return button
 
-    def test_going_to_history_page(self):
-        self.assertInSource('All credits')
-        self.get_search_button()
+    def test_going_to_processed_credits_page(self):
+        self.assertInSource('Processed credits')
+        self.get_filter_button()
 
-    def test_do_a_search_and_make_sure_it_takes_you_back_to_history_page(self):
+    def test_do_filter_and_make_sure_it_takes_you_back_to_processed_page(self):
+        self.get_filter_button().click()
+        self.assertCurrentUrl('/en-gb/processed/')
+
+    def test_filtering_processed_credits(self):
+        self.type_in('id_start', date.today().isoformat())
+        self.get_filter_button().click()
+        self.assertCurrentUrl('/en-gb/processed/')
+        self.assertNotInSource('There was a problem')
+
+
+class SearchCreditsTests(CashbookTestCase):
+    """
+    Tests for search credits
+    """
+
+    def setUp(self):
+        super().setUp()
+        self.login_and_go_to('New credits')
+
+    def get_search_button(self):
+        button = self.driver.find_element_by_xpath('//button[text()="Search"]')
+        self.assertIsNotNone(button)
+        return button
+
+    def test_search_credits(self):
+        search_term = 'Smith'
+        self.type_in('id_search', search_term)
         self.get_search_button().click()
-        self.assertCurrentUrl('/en-gb/history/')
-
-    def test_searching_history(self):
-        self.assertInSource(re.compile(r'\d+ credits? received.'))
-        self.type_in('id_search', '1')
-        self.get_search_button().click()
-        self.assertInSource(re.compile(r'Searching for “1” returned \d+ credits?.'))
-
-    def test_help_popup(self):
-        help_box_heading = self.driver.find_element_by_css_selector('.mtp-disclosure')
-        self.assertCssProperty('.mtp-disclosure__contents', 'display', 'none')
-        self.assertEqual('false', help_box_heading.get_attribute('aria-expanded'))
-        self.click_on_link('Help')
-        self.assertCssProperty('.mtp-disclosure__contents', 'display', 'block')
-        self.assertEqual('true', help_box_heading.get_attribute('aria-expanded'))
-        self.click_on_link('Help')
-        self.assertCssProperty('.mtp-disclosure__contents', 'display', 'none')
-        self.assertEqual('false', help_box_heading.get_attribute('aria-expanded'))
+        self.assertCurrentUrl('/en-gb/search/')
+        self.assertInSource('Search for “%s”' % search_term)
 
 
 @silence_logger(name='mtp', level=logging.WARNING)
@@ -377,11 +216,11 @@ class AdminPagesTests(CashbookTestCase):
     """
     Tests for Admin pages
     """
+    username = 'test-hmp-leeds-ua'
 
     def setUp(self):
         super().setUp()
-        self.login('test-prison-1-ua', 'test-prison-1-ua')
-        self.click_on_text('Manage users')
+        self.login_and_go_to('Manage users')
 
     def _create_dummy_user(self, username):
         self.click_on_text('Manage users')
