652752e162f06f8f2365f4f77891876ab98d07e4
Igor <igor.ushkarev@digital.justice.gov.uk>
2017-11-28 12:54:05 +0000

    Add footer links between cashbook apps and convert context processors into request middleware as those per-request settings are needed in places outside of templates too

--- mtp_cashbook/apps/disbursements/__init__.py
+++ mtp_cashbook/apps/disbursements/__init__.py
@@ -1,22 +1,14 @@
 from functools import wraps
 
-from django.conf import settings
 from django.shortcuts import redirect
-from django.urls import reverse_lazy
-
-
-def disbursements_available(request):
-    return any((
-        prison['nomis_id'] in settings.DISBURSEMENT_PRISONS
-        for prison in request.user.user_data.get('prisons', [])
-    ))
+from django.urls import reverse
 
 
 def disbursements_available_required(f):
     @wraps(f)
     def inner(request, *args, **kwargs):
-        if disbursements_available(request):
+        if request.disbursements_available:
             return f(request, *args, **kwargs)
         else:
-            return redirect(reverse_lazy('home'))
+            return redirect(reverse('home'))
     return inner
