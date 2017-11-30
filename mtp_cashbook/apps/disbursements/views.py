ffbb95377a94f312f2bcb992ab2b8fa7808be6d8
Katy600 <katherine.mccann@digital.justice.gov.uk>
2017-11-30 16:36:51 +0000

    change Send money out to Digital disbursments

--- mtp_cashbook/apps/disbursements/views.py
+++ mtp_cashbook/apps/disbursements/views.py
@@ -52,7 +52,7 @@ class DisbursementView(View):
 
     def dispatch(self, request, *args, **kwargs):
         request.proposition_app = {
-            'name': _('Send money out'),
+            'name': _('Disbursments'),
             'url': build_view_url(self.request, DisbursementStartView.url_name),
         }
         for view in self.get_previous_views(self):
