d6dbe0379cf8611d7fd2f96e3cadfb1ede4b8226
Igor <igor.ushkarev@digital.justice.gov.uk>
2017-12-19 14:22:45 +0000

    Update dependencies and use class-based feedback view

--- mtp_cashbook/apps/feedback/urls.py
+++ mtp_cashbook/apps/feedback/urls.py
@@ -1,23 +1,8 @@
-from django.conf import settings
 from django.conf.urls import url
-from django.urls import reverse_lazy
-from zendesk_tickets import views
 
-from .forms import PrisonTicketForm
+from feedback.views import GetHelpView, GetHelpSuccessView
 
-ticket_subject = 'MTP Cashbook Feedback'
-ticket_tags = ['feedback', 'mtp', 'cashbook', settings.ENVIRONMENT]
 urlpatterns = [
-    url(r'^feedback/$', views.ticket,
-        {
-            'form_class': PrisonTicketForm,
-            'template_name': 'mtp_common/feedback/submit_feedback.html',
-            'success_redirect_url': reverse_lazy('feedback_success'),
-            'subject': ticket_subject,
-            'tags': ticket_tags,
-        }, name='submit_ticket'),
-    url(r'^feedback/success/$', views.success,
-        {
-            'template_name': 'mtp_common/feedback/success.html',
-        }, name='feedback_success'),
+    url(r'^feedback/$', GetHelpView.as_view(), name='submit_ticket'),
+    url(r'^feedback/success/$', GetHelpSuccessView.as_view(), name='feedback_success'),
 ]
---
+++ mtp_cashbook/apps/feedback/views.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.urls import reverse_lazy
+from mtp_common.views import GetHelpView as BaseGetHelpView, GetHelpSuccessView as BaseGetHelpSuccessView
+
+from feedback.forms import PrisonTicketForm
+
+
+class GetHelpView(BaseGetHelpView):
+    form_class = PrisonTicketForm
+    success_url = reverse_lazy('feedback_success')
+    template_name = 'mtp_common/feedback/submit_feedback.html'
+    ticket_subject = 'MTP Cashbook Feedback'
+    ticket_tags = ['feedback', 'mtp', 'cashbook', settings.ENVIRONMENT]
+
+
+class GetHelpSuccessView(BaseGetHelpSuccessView):
+    pass
