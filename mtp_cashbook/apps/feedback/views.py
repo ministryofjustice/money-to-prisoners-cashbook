5d298747ae9febc78fee2247b27143d97375ac2f
Igor <igor.ushkarev@digital.justice.gov.uk>
2018-01-05 11:47:21 +0000

    Rename get-help page as "Contact us" but keep nav tab as "Get help"

--- mtp_cashbook/apps/feedback/views.py
+++ mtp_cashbook/apps/feedback/views.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
 from mtp_common.views import GetHelpView as BaseGetHelpView, GetHelpSuccessView as BaseGetHelpSuccessView
 
 from feedback.forms import PrisonTicketForm
@@ -11,6 +12,12 @@ class GetHelpView(BaseGetHelpView):
     ticket_subject = 'MTP Cashbook Feedback'
     ticket_tags = ['feedback', 'mtp', 'cashbook', settings.ENVIRONMENT]
 
+    def get_context_data(self, **kwargs):
+        kwargs['get_help_title'] = _('Contact us')
+        return super().get_context_data(**kwargs)
+
 
 class GetHelpSuccessView(BaseGetHelpSuccessView):
-    pass
+    def get_context_data(self, **kwargs):
+        kwargs['get_help_title'] = _('Contact us')
+        return super().get_context_data(**kwargs)
