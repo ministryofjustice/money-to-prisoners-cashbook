2ef550b866e5d9b0bae3faa94c152dd403367069
Igor <igor.ushkarev@digital.justice.gov.uk>
2018-01-19 11:54:37 +0000

    Remove disbursement pilot preview process overview page

--- mtp_cashbook/apps/cashbook/utils.py
+++ mtp_cashbook/apps/cashbook/utils.py
@@ -8,7 +8,6 @@ class CashbookMiddleware:
     def __call__(self, request):
         request.pre_approval_required = self.pre_approval_required(request)
         request.disbursements_available = self.disbursements_available(request)
-        request.disbursements_preview = self.disbursements_preview(request)
         return self.get_response(request)
 
     def pre_approval_required(self, request):
@@ -20,11 +19,5 @@ class CashbookMiddleware:
     def disbursements_available(self, request):
         return request.user.is_authenticated and any(
             prison['nomis_id'] in settings.DISBURSEMENT_PRISONS
-            for prison in request.user.user_data.get('prisons', [])
-        )
-
-    def disbursements_preview(self, request):
-        return request.user.is_authenticated and any(
-            prison['nomis_id'] in settings.DISBURSEMENT_PREVIEW_PRISONS
             for prison in request.user.user_data.get('prisons', [])
         )
