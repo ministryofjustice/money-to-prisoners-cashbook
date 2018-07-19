7dae3d89e94f700d696024f94d17549d61454867
Igor <igor.ushkarev@digital.justice.gov.uk>
2018-07-19 16:36:19 +0100

    Remove old-style table sorting

--- mtp_cashbook/apps/cashbook/templatetags/credits.py
+++ mtp_cashbook/apps/cashbook/templatetags/credits.py
@@ -3,7 +3,6 @@ import datetime
 from django import template
 from django.utils import timezone
 from django.utils.dateparse import parse_datetime, parse_date
-from django.utils.http import urlencode
 from django.utils.text import slugify
 
 register = template.Library()
@@ -52,35 +51,3 @@ def dayssince(date):
 @register.filter
 def prefixed_slug(value, prefix='item-'):
     return '%s%s' % (prefix, slugify(value))
-
-
-@register.filter
-def ordering_classes(data, ordering):
-    current_ordering = data.get('ordering')
-    if current_ordering == ordering:
-        return 'mtp-results-ordering mtp-results-ordering--asc'
-    if current_ordering == '-%s' % ordering:
-        return 'mtp-results-ordering mtp-results-ordering--desc'
-    return 'mtp-results-ordering'
-
-
-@register.inclusion_tag('cashbook/includes/result-ordering-for-screenreader.html')
-def describe_ordering_for_screenreader(data, ordering):
-    current_ordering = data.get('ordering')
-    if current_ordering == ordering:
-        ordering = 'ascending'
-    elif current_ordering == '-%s' % ordering:
-        ordering = 'descending'
-    else:
-        ordering = None
-    return {'ordering': ordering}
-
-
-@register.filter
-def query_string_with_reversed_ordering(data, ordering):
-    data = {key: value for key, value in data.items() if value and key != 'page'}
-    current_ordering = data.get('ordering')
-    if current_ordering == ordering:
-        ordering = '-%s' % ordering
-    data['ordering'] = ordering
-    return urlencode(data, doseq=True)
