6576ba404b8d2bc8aad70554febc4bcce9e80519
Igor <igor.ushkarev@digital.justice.gov.uk>
2019-12-03 21:42:24 +0000

    Remove Cloud Platform migration middleware

--- mtp_cashbook/settings/base.py
+++ mtp_cashbook/settings/base.py
@@ -70,7 +70,6 @@ ROOT_URLCONF = 'mtp_cashbook.urls'
 MIDDLEWARE = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
-    'mtp_common.cp_migration.middleware.CloudPlatformMigrationMiddleware',
     'django.middleware.common.CommonMiddleware',
     'mtp_common.auth.csrf.CsrfViewMiddleware',
     'mtp_common.auth.middleware.AuthenticationMiddleware',
