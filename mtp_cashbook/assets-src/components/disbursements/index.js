aa4921ca6b5d21ca60e2dbea44f71ff06e2cc6f7
Igor <igor.ushkarev@digital.justice.gov.uk>
2021-02-02 19:01:18 +0000

    Update disbursement address form view

--- mtp_cashbook/assets-src/components/disbursements/index.js
+++ mtp_cashbook/assets-src/components/disbursements/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 export var Disbursements = {
-  init: function() {
+  init: function () {
 
     // update NOMIS balances without page reload
     $('.mtp-accounts-table tfoot a').click(function (e) {
@@ -18,6 +18,27 @@ export var Disbursements = {
         });
       }).fail(function () {
         $dataBoxes.text(errorMessage);
+      });
+    });
+
+    // fills recipient address form from select field
+    $('.mtp-select__address').each(function () {
+      var $addressSelect = $(this);
+
+      var $addressLine1 = $('#id_address_line1');
+      var $addressLine2 = $('#id_address_line2');
+      var $city = $('#id_city');
+      var $postcode = $('#id_postcode');
+
+      $addressSelect.change(function () {
+        if ($addressSelect.val()) {
+          var $selectedAddress = $('option:selected', $addressSelect);
+          var addressData = $selectedAddress.data('address');
+          $addressLine1.val(addressData['address_line1']);
+          $addressLine2.val(addressData['address_line2']);
+          $city.val(addressData['city']);
+          $postcode.val(addressData['postcode']);
+        }
       });
     });
 
