e78fb9679fa7ecd61ff35bf74b71f70b2adead07
Igor <igor.ushkarev@digital.justice.gov.uk>
2021-02-02 17:33:05 +0000

    Update disbursement amount form view

--- mtp_cashbook/assets-src/components/disbursements/index.js
+++ mtp_cashbook/assets-src/components/disbursements/index.js
@@ -2,9 +2,29 @@
 
 export var Disbursements = {
   init: function() {
+
+    // update NOMIS balances without page reload
+    $('.mtp-accounts-table tfoot a').click(function (e) {
+      e.preventDefault();
+      var $button = $(this);
+      var $dataBoxes = $('.mtp-accounts-table__amount');
+      var errorMessage = django.gettext('Please try again later');
+      $dataBoxes.text(django.gettext('Please wait…'));
+      $.ajax($button.attr('href')).done(function (response) {
+        $dataBoxes.each(function () {
+          var $dataBox = $(this);
+          var data = response[$dataBox.data('balance')];
+          $dataBox.text(data === undefined || data === null ? errorMessage : data);
+        });
+      }).fail(function () {
+        $dataBoxes.text(errorMessage);
+      });
+    });
+
     // prevent new lines in wrapped textareas
     $('#id_remittance_description').keydown(function (e) {
       return e.keyCode !== 13;
     });
+
   }
 };
