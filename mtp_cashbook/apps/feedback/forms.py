887efb31c283d69e4bf4ddd48370c3a13e55482c
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2021-03-23 12:21:15 +0000

    MTP-1838: Simplify 'Contact us' form
    - Remove lead paragraph
    - Changed textarea label to 'Please give details'
    
    Also
    - Made 'Your email address' field mandatory.
    
    Ticket: https://dsdmoj.atlassian.net/browse/MTP-1838

--- mtp_cashbook/apps/feedback/forms.py
+++ mtp_cashbook/apps/feedback/forms.py
@@ -1,10 +1,22 @@
+from django import forms
 from django.utils.text import slugify
+from django.utils.translation import gettext_lazy as _
 from mtp_common.auth.exceptions import Unauthorized
 from slumber.exceptions import HttpClientError
 from zendesk_tickets.forms import EmailTicketForm
 
 
 class PrisonTicketForm(EmailTicketForm):
+
+    ticket_content = forms.CharField(
+        label=_('Please give details'),
+        widget=forms.Textarea,
+    )
+
+    contact_email = forms.EmailField(
+        label=_('Your email address'), required=True,
+    )
+
     def submit_ticket(self, request, subject, tags, ticket_template_name, **kwargs):
         try:
             accessible_prisons = sorted(prison['name'] for prison in request.user.user_data.get('prisons', []))
