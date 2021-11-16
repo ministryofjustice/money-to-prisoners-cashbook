8c23985b163a70ea89f9a2d5597692db6e2155e0
Igor <igor.ushkarev@digital.justice.gov.uk>
2021-11-16 23:03:03 +0000

    Add method to delete a user flag

--- mtp_cashbook/utils.py
+++ mtp_cashbook/utils.py
@@ -10,3 +10,14 @@ def add_user_flag(request, flag):
     flags = list(flags)
     request.user.user_data['flags'] = flags
     request.session[USER_DATA_SESSION_KEY] = request.user.user_data
+
+
+def delete_user_flag(request, flag):
+    api_session = get_api_session(request)
+    api_session.delete(f'/users/{request.user.username}/flags/{flag}/')
+    flags = set(request.user.user_data.get('flags') or [])
+    if flag in flags:
+        flags.remove(flag)
+    flags = list(flags)
+    request.user.user_data['flags'] = flags
+    request.session[USER_DATA_SESSION_KEY] = request.user.user_data
