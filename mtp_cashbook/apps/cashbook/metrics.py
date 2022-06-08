5dd9362db275f8243c32dc05e6e37a51377300a4
Igor <igor.ushkarev@digital.justice.gov.uk>
2022-06-08 16:47:31 +0100

    Replace prometheus counters with regular logging …because counters are local to a process and 1) credits are credited asynchronously; therefore never in a process that serves prometheus metrics 2) disbursement counters are synchronous but reset whenever a process spawns so a log message is actually more useful/durable

--- mtp_cashbook/apps/cashbook/metrics.py
+++
@@ -1,7 +0,0 @@
-from django.apps import apps
-from prometheus_client import Summary
-
-credited_summary = Summary('mtp_cashbook_credited', 'Credits credited')
-
-app = apps.get_app_config('metrics')
-app.register_collector(credited_summary)
